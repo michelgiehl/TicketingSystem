1dc1c4746c38345581198f32633dfba12693b69e
realUgunity <michel.giehl@gmail.com>
2017-02-14 16:33:02 +0100

    Add files via upload

---
+++ style/css/dark-theme/js/bootstrap-extra.js
@@ -0,0 +1,9 @@
+//Import von Bootstrap Tooltips
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+
+//Import von Bootstrap Popover
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+});
